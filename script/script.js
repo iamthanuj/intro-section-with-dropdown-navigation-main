f1440de34a1ee11ccf1d5d78d725892511156e08
iamthanuj <thanujdevil@gmail.com>
2023-07-21 02:54:14 +0530

    Fix Sub-menus

--- script/script.js
+++ script/script.js
@@ -1,12 +1,23 @@
-document.querySelector('.js-features-menu').addEventListener('click',()=>{
-    const dropMenu = document.querySelector('.js-dropdown-menu');
-    if(dropMenu.classList.contains('dropdown-active')){
+
+
+
+
+
+document.querySelector('.js-features-menu').addEventListener('click', () => {
+    dropDown('features')
+})
+
+document.querySelector('.js-company-menu').addEventListener('click',()=>{
+    dropDown('company')
+})
+
+
+function dropDown(menu) {
+    const dropMenu = document.querySelector(`.js-${menu}-dropdown-menu`);
+    if (dropMenu.classList.contains('dropdown-active')) {
         dropMenu.classList.remove('dropdown-active')
     }
     else {
         dropMenu.classList.add('dropdown-active')
     }
-
-
-    // .classList.add('dropdown-active');
-})
+}
